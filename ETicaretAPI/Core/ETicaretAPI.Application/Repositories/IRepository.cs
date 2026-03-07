69c886ed61f9e587cccbc1a174f2f998e6a8a2b7
brkolgc <burakolgac34@gmail.com>
2026-03-07 23:50:26 +0300

    Build Repository Design Pattern

---
+++ ETicaretAPI/Core/ETicaretAPI.Application/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Repositories
+{
+    public interface IRepository<T> where T : BaseEntity 
+    {
+        DbSet<T> Table { get; }
+    }
+}
