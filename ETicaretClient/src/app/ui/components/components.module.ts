d9541b5ff0131f8707d3da1599bbb3f7528e8fa3
brkolgc <burakolgac34@gmail.com>
2026-03-12 02:39:49 +0300

    User Register Form with Reactive Forms (Angular)

--- ETicaretClient/src/app/ui/components/components.module.ts
+++ ETicaretClient/src/app/ui/components/components.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ProductsModule } from './products/products.module';
 import { BasketsModule } from './baskets/baskets.module';
 import { HomeModule } from './home/home.module';
+import { RegisterComponent } from './register/register.component';
+import { RegisterModule } from './register/register.module';
 
 
 
@@ -12,7 +14,8 @@ import { HomeModule } from './home/home.module';
     CommonModule,
     ProductsModule,
     BasketsModule,
-    HomeModule
+    HomeModule,
+    RegisterModule
   ]
 })
 export class ComponentsModule { }
